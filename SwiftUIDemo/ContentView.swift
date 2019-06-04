a3d6965c0316e84110da242782dd759f55eeae87
TyroneZhang <tyronezhang.seeyou@gmail.com>
2019-06-04 14:13:11 +0800

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Demon_Yao on 2019/6/4.
+//  Copyright © 2019 Tyrone Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
